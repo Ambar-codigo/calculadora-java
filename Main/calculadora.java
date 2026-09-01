d47394a9503f778776258dafe3de09be5434c70f
aquelequeperguntou <215685909+lyla1311@users.noreply.github.com>
2026-09-01 11:35:56 -0300

    adição
    escrita de código adição

--- Main/calculadora.java
+++ Main/calculadora.java
@@ -1,2 +1,14 @@
 
 //codigo de adicao
+
+javapublic class Adicao {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+        
+        // Operação aditiva
+        int resultado = numero1 + numero2;
+        
+        System.out.println("O resultado da soma é: " + resultado);
+    }
+}
