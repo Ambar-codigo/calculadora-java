b54f4e74e7fc930aa229d27117d5fc2a72c2e70e
Maria_Cedup <mariasm13112008@gmail.com>
2026-09-22 11:31:19 -0300

    Update calculadora.java

--- Main/calculadora.java
+++ Main/calculadora.java
@@ -12,3 +12,8 @@ javapublic class Adicao {
         System.out.println("O resultado da soma é: " + resultado);
     }
 }
+
+public static void Subtracao(double a, double b) {
+        double resultado = a - b;
+        System.out.println("\n[Subtração] Resultado: " + a + " - " + b + " = " + resultado);
+    }
